2658a7ea8140b6b08f8abb4b5da7bdb7becef66c
Jason van den Berg <jayvdb1@gmail.com>
2021-09-28 16:21:08 +0200

    User keypair generated

--- SlashtagsServer.tsx
+++ SlashtagsServer.tsx
@@ -1,6 +1,10 @@
 import nodejs from 'nodejs-mobile-react-native';
 
-///import EventEmitter = from 'react-native/Libraries/vendor/emitter/EventEmitter';
+///import EventEmitter from 'react-native/Libraries/vendor/emitter/EventEmitter';
+
+// "@synonymdev/slashtags-auth": "^0.1.4",
+//   "@synonymdev/slashtags-url": "^0.1.1",
+//   "noise-curve-secp256k1": "git+ssh://git@github.com/synonymdev/noise-curve-tiny-secp256k1.git"
 
 import React, {useState, useEffect} from 'react';
 import {
@@ -18,6 +22,8 @@ import {
   Responder,
 } from '@synonymdev/slashtags-auth/types/interfaces';
 
+import {SlashtagsURL} from '@synonymdev/slashtags-url/types/interfaces';
+
 // import {createAuth} from '@synonymdev/slashtags-auth';
 // import {secp256k1 as curve} from 'noise-curve-tiny-secp';
 // import * as SlashtagsURL from '@synonymdev/slashtags-url';
@@ -32,7 +38,9 @@ const Wallet = () => {
 
   const [severStarted, setServerStarted] = useState(false);
   const [message, setMessage] = useState('');
-  const [decodedUrl, setDecodedUrl] = useState({});
+  const [decodedUrl, setDecodedUrl] = useState<SlashtagsURL>();
+  const [keyPair, setKeyPair] = useState<KeyPair>();
+  const [authPayload, setAuthPayload] = useState<object>();
 
   useEffect(() => {
     if (severStarted) {
@@ -44,28 +52,68 @@ const Wallet = () => {
 
     nodejs.channel.addListener('message', setMessage, null);
     nodejs.channel.addListener('decode-url', setDecodedUrl, null);
+    nodejs.channel.addListener('key-pair', setKeyPair, null);
   }, [severStarted]);
+
+  useEffect(() => {
+    // Handle incoming action urls
+    (async () => {
+      if (!decodedUrl) {
+        return;
+      }
+
+      try {
+        const {actionID, payload} = decodedUrl!;
+
+        switch (actionID) {
+          case 'b2iaqaamaaqjcbw5htiftuksya3xkgxzzhrqwz4qtk6oxn7u74l23t2fthlnx3ked':
+            setAuthPayload(payload);
+            break;
+
+          default:
+            setAuthPayload(undefined);
+            console.error('Unknown action');
+            return;
+        }
+      } catch (error) {
+        console.error('Invalid actionURL url');
+      }
+    })();
+  }, [decodedUrl, actionURL]);
 
   return (
     <View>
-      <Button title={'Start main.js'} onPress={() => {}} />
+      <TextInput
+        style={{height: 20, width: '100%', backgroundColor: 'blue'}}
+        value={actionURL}
+        onChangeText={setActionURL}
+      />
 
       <Button
         title="Decode URL"
         onPress={() => nodejs.channel.post('decode-url', actionURL)}
       />
 
-      <TextInput
-        style={{height: 20, width: '100%', backgroundColor: 'red'}}
-        value={actionURL}
-        onChangeText={setActionURL}
+      <Button
+        title="Set user"
+        onPress={() => nodejs.channel.post('key-pair', 'My first user')}
       />
 
       {/*<Button title={'decodeURL'} onPress={decodeURL} />*/}
 
-      <Text>{message}</Text>
+      <Text>
+        Server message: {JSON.stringify(message)}
+        {'\n\n'}
+      </Text>
 
-      <Text>URL: {JSON.stringify(decodedUrl)}</Text>
+      {/*<Text>URL: {JSON.stringify(decodedUrl)}</Text>*/}
+
+      <Text>
+        AUTH: {JSON.stringify(authPayload)}
+        {'\n\n'}
+      </Text>
+
+      <Text>KEY PAIR: {JSON.stringify(keyPair)}</Text>
     </View>
   );
 };
--- nodejs-assets/nodejs-project/main.js
+++ nodejs-assets/nodejs-project/main.js
@@ -3,6 +3,10 @@
 
 var rn_bridge = require('rn-bridge');
 var SlashtagsURL = require('@synonymdev/slashtags-url');
+// var createAuth = require('@synonymdev/slashtags-auth').createAuth;
+var curve = require('noise-curve-tiny-secp').secp256k1;
+
+var initiator;
 
 // Echo every message received from react-native.
 rn_bridge.channel.on('message', msg => {
@@ -13,6 +17,19 @@ rn_bridge.channel.on('decode-url', actionURL => {
   const decoded = SlashtagsURL.parse(actionURL);
 
   rn_bridge.channel.post('decode-url', decoded);
+});
+
+rn_bridge.channel.on('key-pair', seed => {
+  const username = seed;
+  const userKeyPair = curve.generateSeedKeyPair(seed);
+  //
+  // const {initiator: init} = createAuth(userKeyPair, {
+  //   metadata: {preferred_name: username},
+  // });
+  //
+  // initiator = init;
+
+  rn_bridge.channel.post('key-pair', userKeyPair);
 });
 
 // Inform react-native node is initialized.
